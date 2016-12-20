104bb2c7ed721554e8aaa67fd2d4bc09743baf9a
Nathan Cowieson <nathan.cowieson@diamond.ac.uk>
2016-12-20 16:07:25 +0100

    first commit

---
+++ Events_files/Events.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20161206
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,455),url:'Events_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Events_files/stroke_1.png'},{rect:new IWRect(2,-2,645,4),url:'Events_files/stroke_2.png'},{rect:new IWRect(647,-2,5,4),url:'Events_files/stroke_3.png'},{rect:new IWRect(647,2,5,455),url:'Events_files/stroke_4.png'},{rect:new IWRect(647,457,5,4),url:'Events_files/stroke_5.png'},{rect:new IWRect(2,457,645,4),url:'Events_files/stroke_6.png'},{rect:new IWRect(-2,457,4,4),url:'Events_files/stroke_7.png'}],new IWSize(650,459))});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Events_files/EventsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id5');applyEffects()}
