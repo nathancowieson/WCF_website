8be998d8836d06a9cda9a8cd94c17a9debd1380c
Nathan Cowieson <nathan.cowieson@diamond.ac.uk>
2016-12-20 16:20:37 +0100

    first commit

---
+++ Lifted_files/Lifted.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20161213
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Lifted_files/LiftedMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
