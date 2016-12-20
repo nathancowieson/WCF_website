48543d9f370baec109e650225e9f45e55d047136
Nathan Cowieson <nathan.cowieson@diamond.ac.uk>
2016-12-20 16:29:52 +0100

    first commit

---
+++ Rotas_files/Rotas.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20161206
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Rotas_files/RotasMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
