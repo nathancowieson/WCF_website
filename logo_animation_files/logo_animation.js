0066eb9eee1451cf6c83884503e14506a989d84b
Nathan Cowieson <nathan.cowieson@diamond.ac.uk>
2016-12-20 16:21:29 +0100

    first commit

---
+++ logo_animation_files/logo_animation.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20161206
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('logo_animation_files/logo_animationMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
