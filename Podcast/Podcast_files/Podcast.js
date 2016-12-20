8a3475941fa8c19efb55db8e336242aa6211c7be
Nathan Cowieson <nathan.cowieson@diamond.ac.uk>
2016-12-20 16:29:12 +0100

    first commit

---
+++ Podcast/Podcast_files/Podcast.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20161206
+
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("http://www.wantagechristianfellowship.org.uk/WCF/Podcast/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Podcast_files/PodcastMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
