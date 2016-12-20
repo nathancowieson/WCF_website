96c47fb75efe243873f50586e450ab21b897dd4f
Nathan Cowieson <nathan.cowieson@diamond.ac.uk>
2016-12-20 16:33:05 +0100

    first commit

---
+++ Speaking_plan_files/Speaking_plan.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20161206
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Speaking_plan_files/Speaking_planMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
