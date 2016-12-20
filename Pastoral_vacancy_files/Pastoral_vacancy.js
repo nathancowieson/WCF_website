bb50abec5799e68bdf301f67ca7d3463a83d954f
Nathan Cowieson <nathan.cowieson@diamond.ac.uk>
2016-12-20 16:27:42 +0100

    first commit

---
+++ Pastoral_vacancy_files/Pastoral_vacancy.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20161206
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Pastoral_vacancy_files/Pastoral_vacancyMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
